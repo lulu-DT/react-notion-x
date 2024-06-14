d1640efb7471454a7fb197e2793cc4a00eef9dba
lulu-DT <77606026+lulu-DT@users.noreply.github.com>
2024-06-14 11:32:38 +0800

    Update config.ts

--- examples/full/lib/config.ts
+++ examples/full/lib/config.ts
@@ -1,8 +1,8 @@
 // TODO: change these to your own values
 // NOTE: rootNotionSpaceId is optional; set it to undefined if you don't want to
 // use it.
-export const rootNotionPageId = '067dd719a912471ea9a3ac10710e7fdf'
-export const rootNotionSpaceId = 'fde5ac74-eea3-4527-8f00-4482710e1af3'
+export const rootNotionPageId = 'Blog-ea556f038e3a46d48ee85f3f9d00d309'
+export const rootNotionSpaceId = 'Blog-ea556f038e3a46d48ee85f3f9d00d309'
 
 // NOTE: having this enabled can be pretty expensive as it re-generates preview
 // images each time a page is built. In a production setting, we recommend that
